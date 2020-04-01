79e711251ec43c5cbfb3a84585321d2ee87b5459
UOTOGK <939458466@qq.com>
2020-04-01 21:12:43 +0800

    cctv

--- cctvxinwen.py
+++ cctvxinwen.py
@@ -13,33 +13,28 @@ class Weibospider:
     def __init__(self):
         # 获取首页的相关信息：
         self.start_url = 'https://weibo.com/cctvxinwen?profile_ftype=1&is_all=1#_0'
-
+        self.domain="100406"
+        self.uuid="1537790411"
         self.headers = {
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "accept-encoding": "gzip, deflate, br",
             "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
             "cache-control": "max-age=0",
             "cookie": "UM_distinctid=16ebb2a69e87c9-0f9e77de80db66-7373e61-1fa400-16ebb2a69e946c; SINAGLOBAL=9078084295180.92.1575098149469; wb_timefeed_6690784751=1; un=17603072726; ALF=1616915411; SCF=AudfR6nptB5k5GPk2ngKX-Z921MsZz1mk6ZCW1y-e2Dcf2L-kfpApyRwSSfnZR7PY-jnJZLjvyww2feOwq079SA.; SUHB=0y0ZcWLCI9543c; ULV=1585379421606:3:3:3:7266807228602.845.1585379421548:1585370401285; UOR=www.baidu.com,vdisk.weibo.com,www.baidu.com; YF-Page-G0=8a1a69dc6ba21f1cd10b039dff0f4381|1585497087|1585497087; SUB=_2AkMp3Es3f8NxqwJRmfoTzG_lbY5yzwvEieKfgLrsJRMxHRl-yT92qnQEtRB6Alxl2FqAT8K5PGjwZBGo92zjhCu5p8nO; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5HWPXBcydOZ1KeChIjlHJj",  # 此处填入自己的cookie
-            "referer": "https://www.weibo.com/u/1537790411?topnav=1&wvr=6&topsug=1",
-            # https://s.weibo.com/weibo/%E4%BA%BA%E6%B0%91%E6%97%A5%E6%8A%A5?topnav=1&wvr=6&topsug=1
+            "referer": "https://www.weibo.com/u/"+self.uuid+"?topnav=1&wvr=6&topsug=1",
             "upgrade-insecure-requests": "1",
-            # 此处填入自己的User agent
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
         }
-        self.proxy = {
-            # 此处填入自己的proxy
-        }
+       
 
     def parse_home_url(self, url):  # 处理解析首页面的详细信息（不包括两个通过ajax获取到的页面）
         res = requests.get(url, headers=self.headers)
         response = res.content.decode().replace("\\", "")
-        # every_url = re.compile('target="_blank" href="(/\d+/\w+\?from=\w+&wvr=6&mod=weibotime)"', re.S).findall(response)
-        every_id = re.compile(
-            'name=(\d+)', re.S).findall(response)  # 获取次级页面需要的id
+        every_id = re.compile('name=(\d+)', re.S).findall(response)  # 获取次级页面需要的id
+        every_link = re.compile('<a \s*name=\d+.*?href="(.*?)".*?>', re.S).findall(response)    
         home_url = []
         for id in every_id:
             base_url = 'https://weibo.com/aj/v6/comment/big?ajwvr=6&id={}&from=singleWeiBo'
-            #           https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4375226604955403&root_comment_max_id=913579016927848&root_comment_max_id_type=0&root_comment_ext_param=&page=2&filter=hot&sum_comment_number=6978&filter_tips_before=0&from=singleWeiBo&__rnd=1580371869678
             url = base_url.format(id)
             home_url.append(url)
         return home_url
@@ -77,11 +72,9 @@ class Weibospider:
                 f.write("\n")
 
     def run(self):
-        start_url = 'https://weibo.com/u/1537790411?page={}&is_all=1'
-        start_ajax_url1 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100406&is_all=1&page={0}&pagebar=0&pl_name=Pl_Official_MyProfileFeed__20&id=1004061537790411&script_uri=/u/1537790411&pre_page={0}'
-        # https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100406&page=1&is_all=1&pagebar=0&pl_name=Pl_Official_MyProfileFeed__20&id=1004061537790411&script_uri=/u/1537790411&feed_type=0&pre_page=1&domain_op=100406&__rnd=1580360591628
-        # https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100406&profile_ftype=1&is_all=1&pagebar=0&pl_name=Pl_Official_MyProfileFeed__20&id=1004061537790411&script_uri=/p/1004061537790411/home&feed_type=0&page=1&pre_page=1&domain_op=100406&__rnd=1580372698884
-        start_ajax_url2 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100406&is_all=1&page={0}&pagebar=1&pl_name=Pl_Official_MyProfileFeed__20&id=1004061537790411&script_uri=/u/1537790411&pre_page={0}'
+        start_url = 'https://weibo.com/u/'+self.uuid+'?page={}&is_all=1'
+        start_ajax_url1 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain='+self.domain+'&is_all=1&page={0}&pagebar=0&pl_name=Pl_Official_MyProfileFeed__20&id='+self.domain+''+self.uuid+'&script_uri=/u/'+self.uuid+'&pre_page={0}'
+        start_ajax_url2 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain='+self.domain+'&is_all=1&page={0}&pagebar=1&pl_name=Pl_Official_MyProfileFeed__20&id='+self.domain+''+self.uuid+'&script_uri=/u/'+self.uuid+'&pre_page={0}'
         for i in range(12):  # 微博共有12页
             home_url = self.parse_home_url(start_url.format(i + 1))  # 获取每一页的微博
             ajax_url1 = self.parse_home_url(
@@ -95,7 +88,7 @@ class Weibospider:
             print(all_url)
             for j in range(len(all_url)):
                 # print(all_url[j])
-                path_name = "luhan第{}条微博相关评论.txt".format(i * 45 + j + 1)
+                path_name = "cctvxinwen第{}条微博相关评论.txt".format(i * 45 + j + 1)
                 all_count, comment_info_list = self.parse_comment_info(
                     all_url[j])
                 self.write_file(path_name, comment_info_list)
