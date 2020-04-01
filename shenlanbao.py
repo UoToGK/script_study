6bfd5ac870a772c5abf7f66aa9c2df049aeea5c5
UOTOGK <939458466@qq.com>
2020-04-01 21:53:12 +0800

    ok

--- shenlanbao.py
+++ shenlanbao.py
@@ -21,7 +21,7 @@ class Weibospider:
             "accept-encoding": "gzip, deflate, br",
             "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
             "cache-control": "max-age=0",
-            "cookie": "UM_distinctid=16ebb2a69e87c9-0f9e77de80db66-7373e61-1fa400-16ebb2a69e946c; SINAGLOBAL=9078084295180.92.1575098149469; wb_timefeed_6690784751=1; un=17603072726; ALF=1616915411; SCF=AudfR6nptB5k5GPk2ngKX-Z921MsZz1mk6ZCW1y-e2Dcf2L-kfpApyRwSSfnZR7PY-jnJZLjvyww2feOwq079SA.; SUHB=0y0ZcWLCI9543c; ULV=1585379421606:3:3:3:7266807228602.845.1585379421548:1585370401285; UOR=www.baidu.com,vdisk.weibo.com,www.baidu.com; YF-Page-G0=8a1a69dc6ba21f1cd10b039dff0f4381|1585497087|1585497087; SUB=_2AkMp3Es3f8NxqwJRmfoTzG_lbY5yzwvEieKfgLrsJRMxHRl-yT92qnQEtRB6Alxl2FqAT8K5PGjwZBGo92zjhCu5p8nO; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5HWPXBcydOZ1KeChIjlHJj",  # 此处填入自己的cookie
+            "cookie": "UM_distinctid=16ebb2a69e87c9-0f9e77de80db66-7373e61-1fa400-16ebb2a69e946c; SINAGLOBAL=9078084295180.92.1575098149469; wb_timefeed_6690784751=1; ALF=1616915411; SCF=AudfR6nptB5k5GPk2ngKX-Z921MsZz1mk6ZCW1y-e2Dcf2L-kfpApyRwSSfnZR7PY-jnJZLjvyww2feOwq079SA.; SUHB=0y0ZcWLCI9543c; ULV=1585379421606:3:3:3:7266807228602.845.1585379421548:1585370401285; UOR=www.baidu.com,vdisk.weibo.com,www.baidu.com;  SUB=_2AkMp3Es3f8NxqwJRmfoTzG_lbY5yzwvEieKfgLrsJRMxHRl-yT92qnQEtRB6Alxl2FqAT8K5PGjwZBGo92zjhCu5p8nO; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5HWPXBcydOZ1KeChIjlHJj",  # 此处填入自己的cookie
             "referer": "https://weibo.com/u/6283856714?profile_ftype=1&is_all=1",
             "upgrade-insecure-requests": "1",
             # 此处填入自己的User agent
@@ -34,9 +34,6 @@ class Weibospider:
     def parse_home_url(self, url):  # 处理解析首页面的详细信息（不包括两个通过ajax获取到的页面）
         res = requests.get(url, headers=self.headers)
         response = res.content.decode().replace("\\", "")
-        with open('./1.html', "a", encoding="UTF-8") as f:
-            f.write(response) 
-            f.write("\n")
         every_id = re.compile(
             'name=(\d+)', re.S).findall(response)  # 获取次级页面需要的id
         every_link = re.compile(
@@ -50,6 +47,25 @@ class Weibospider:
             home_url.append(url)
         return home_url
 
+    def parse_every_link(self, url):  # 处理解析首页面的详细信息（不包括两个通过ajax获取到的页面）
+        res = requests.get(url, headers=self.headers)
+        response = res.content.decode().replace("\\", "")
+        every_link = re.compile(
+            '<a \s*name=\d+.*?href="(.*?)".*?>', re.S).findall(response)  # 获取次级页面需要的href
+        home__link= []
+        for link in every_link:
+            base_url = 'https://weibo.com/'+link
+            home__link.append(base_url)
+        return home__link
+
+    def parse_weibo_info(self, url): 
+        new_url=re.findall(r"\d+\/(.+?)\?from",url)
+        link="https://weibo.com/6283856714/"+new_url[0]+"?type=comment"
+        res = requests.get(link, headers=self.headers)
+        response = res.text
+        with open('./2.html', "a", encoding="UTF-8") as f:
+                f.write(response)
+                f.write("\n")
     def parse_comment_info(self, url):  # 爬取直接发表评论的人的相关信息(name,info,time,info_url)
         res = requests.get(url, headers=self.headers)
         response = res.json()
@@ -87,12 +103,14 @@ class Weibospider:
         start_ajax_url1 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100606&is_all=1&page={0}&pagebar=0&pl_name=Pl_Official_MyProfileFeed__20&id=1006066283856714&script_uri=/u/6283856714&pre_page={0}'
         start_ajax_url2 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100606&is_all=1&page={0}&pagebar=1&pl_name=Pl_Official_MyProfileFeed__20&id=1006066283856714&script_uri=/u/6283856714&pre_page={0}'
         for i in range(12):  # 微博共有12页
+            every_link=self.parse_every_link(start_url.format(i + 1))
             home_url = self.parse_home_url(start_url.format(i + 1))  # 获取每一页的微博
             ajax_url1 = self.parse_home_url(
                 start_ajax_url1.format(i + 1))  # ajax加载页面的微博
             ajax_url2 = self.parse_home_url(
                 start_ajax_url2.format(i + 1))  # ajax第二页加载页面的微博
             all_url = home_url + ajax_url1 + ajax_url2
+            print(every_link)
             print(home_url)
             print(ajax_url1)
             print(ajax_url2)
@@ -102,6 +120,7 @@ class Weibospider:
                 path_name = "深蓝保第{}条微博相关评论.txt".format(i * 45 + j + 1)
                 all_count, comment_info_list = self.parse_comment_info(
                     all_url[j])
+                weibo_info=self.parse_weibo_info(every_link[j])
                 self.write_file(path_name, comment_info_list)
                 for num in range(1, 10000):
                     if num * 15 < int(all_count) + 15:
